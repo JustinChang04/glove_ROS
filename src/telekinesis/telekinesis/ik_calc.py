bd4172dd1e6d984a9d2f2371d84a360d3571ba3f
Justin Chang <chang.justin0106@gmail.com>
2025-02-28 14:57:33 -0800

    Fixed IK

--- src/telekinesis/telekinesis/ik_calc.py
+++ src/telekinesis/telekinesis/ik_calc.py
@@ -27,15 +27,14 @@ class PybulletIK(Node):
         
         self.is_left = self.declare_parameter('isLeft', False).get_parameter_value().bool_value
         self.glove_to_leap_mapping_scale = 1.6
-        self.leapEndEffectorIndex = [1, 3, 5, 7, 9]
+        self.leapEndEffectorIndex = [2, 3, 7, 8, 11, 12, 16, 17, 21, 22]
         
         path_src = os.path.join(os.path.expanduser("~"), "glove_ROS/src/telekinesis/robot_hand/hand.urdf")
-        print(f"Path src = {path_src}")
         ##You may have to set this path for your setup on ROS2
         self.LeapId = p.loadURDF(
             path_src,
-            [-0.05, -0.03, -0.25],
-            p.getQuaternionFromEuler([0, 1.57, 1.57]),
+            [0, 0, 0],
+            p.getQuaternionFromEuler([0, 0, 0]),
             useFixedBase = True
         )
                
@@ -51,8 +50,7 @@ class PybulletIK(Node):
        ##################
 
         self.numJoints = p.getNumJoints(self.LeapId)
-        print(f"Num joints: {self.numJoints}")
-        p.setGravity(0, 0, 0)
+        # p.setGravity(0, 0, 0)
         useRealTimeSimulation = 0
         p.setRealTimeSimulation(useRealTimeSimulation)
         self.create_target_vis()
@@ -80,16 +78,21 @@ class PybulletIK(Node):
         p.changeVisualShape(self.ballMbt[4], -1, rgbaColor=[1, 0, 1, 1])
         
     def update_target_vis(self, hand_pos):
-        for i, joint_idx in enumerate(self.leapEndEffectorIndex): # for every joint that is considered the end 
-            link_state = p.getLinkState(self.robotId, joint_idx) #this gets the specific joint information 
-            position = link_state[0]  # Get the world position of the fingertip joint (in the link_state)
-            p.resetBasePositionAndOrientation(self.ballMbt[i], position, [0, 0, 0, 1])  # sets the position of each of the balls 
-        
+        _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
+        p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos[1], current_orientation)
+        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
+        p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[5], current_orientation)
+        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
+        p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[9], current_orientation)
+        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
+        p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[7], current_orientation)
+        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[4])
+        p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[3], current_orientation)
+
     def get_glove_data(self, pose):
         #gets the data converts it and then computes IK and visualizes
         poses = pose.poses
 
-        print(f"Poses: {len(poses)}")
         hand_pos = []  
         for i in range(0,10):
             hand_pos.append([poses[i].position.x, poses[i].position.y, -poses[i].position.z])
@@ -129,6 +132,8 @@ class PybulletIK(Node):
         rightHandThumb_pos = hand_pos[3]
         
         leapEndEffectorPos = [
+            rightHandThumb_middle_pos,
+            rightHandThumb_pos,
             rightHandIndex_middle_pos,
             rightHandIndex_pos,
             rightHandMiddle_middle_pos,
@@ -136,9 +141,7 @@ class PybulletIK(Node):
             rightHandRing_middle_pos,
             rightHandRing_pos,
             rightHandPinky_middle_pos,
-            rightHandPinky_pos,
-            rightHandThumb_middle_pos,
-            rightHandThumb_pos
+            rightHandPinky_pos
         ]
 
         jointPoses = p.calculateInverseKinematics2(
